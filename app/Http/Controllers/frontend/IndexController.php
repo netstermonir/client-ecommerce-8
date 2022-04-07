1c245131b25e933f50178c478e8e76f419de186c
netstermonir <monirhossain6163@gmail.com>
2022-04-07 22:32:07 +0600

    customer review submit

--- app/Http/Controllers/frontend/IndexController.php
+++ app/Http/Controllers/frontend/IndexController.php
@@ -10,10 +10,6 @@ use App\Models\Review;
 
 class IndexController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware("auth");
-    }
     //index page load method
     public function index(){
         $category = DB::table('categories')->get();
